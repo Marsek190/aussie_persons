6be76dd1a0b0a7063d954c510beaded4396a848a
Dmitriy.Arkhipov <d.arkhipov1997@yandex.ru>
2021-08-29 20:02:48 +0300

    development part three: upgrade repo

--- app/server/src/Customer/Infrastructure/Repository/DbCustomerRepository.php
+++ app/server/src/Customer/Infrastructure/Repository/DbCustomerRepository.php
@@ -3,15 +3,18 @@
 namespace Customer\Infrastructure\Repository;
 
 use Exception;
+use Doctrine\ORM\EntityRepository;
 use Customer\Domain\ValueObject\Customer;
 use Doctrine\ORM\EntityManagerInterface;
 use Customer\Application\Repository\CustomerRepository;
 use Customer\Infrastructure\Repository\Converter\CustomerConverter;
+use Customer\Infrastructure\Repository\Entity\Customer as CustomerEntity;
 
 class DbCustomerRepository implements CustomerRepository
 {
     private EntityManagerInterface $entityManager;
     private CustomerConverter $converter;
+    private EntityRepository $repo;
     private int $batchSize;
 
     public function __construct(EntityManagerInterface $entityManager, CustomerConverter $converter, int $batchSize)
@@ -19,6 +22,7 @@ class DbCustomerRepository implements CustomerRepository
         $this->entityManager = $entityManager;
         $this->converter = $converter;
         $this->batchSize = $batchSize;
+        $this->repo = $this->entityManager->getRepository(CustomerEntity::class);
     }
 
     /** @inheritDoc */
@@ -49,12 +53,12 @@ class DbCustomerRepository implements CustomerRepository
     /** @inheritDoc */
     public function findAll(): array
     {
-        
+        return $this->repo->findAll();
     }
 
     /** @inheritDoc */
     public function findById(int $id): Customer
     {
-
+        return $this->repo->find($id);
     }
 }
